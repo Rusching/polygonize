3b07bc5aaf74ad72b3636b1f40529e66120b68a1
Rusching <2785442070@qq.com>
2023-03-04 22:33:34 +0800

    make codes conform to the pylint rule (direction_fix, svg)

--- direction_fix.py
+++ direction_fix.py
@@ -1,100 +1,105 @@
-# Fix the direction of polygon contour to certain directions
-# by inserting key points between every 2 points
+"""
+Fix the direction of polygon contour to certain directions
+by inserting key points between every 2 points
+"""
+
 
 def fix_direction_octagon(poly):
+    """fix direction to octagon like"""
     # input is a polygon contour
     segments = len(poly)
-    v3s = list()
+    v3s = []
     for i in range(segments):
         # get v1, v2 with v1 is at left (lower x)
         if i != segments-1:
             if poly[i][0] < poly[i + 1][0]:
-                v1 = poly[i]
-                v2 = poly[i + 1]
+                v_1 = poly[i]
+                v_2 = poly[i + 1]
             else:
-                v1 = poly[i + 1]
-                v2 = poly[i]
+                v_1 = poly[i + 1]
+                v_2 = poly[i]
         else:
             if poly[i][0] < poly[0][0]:
-                v1 = poly[i]
-                v2 = poly[0]
+                v_1 = poly[i]
+                v_2 = poly[0]
             else:
-                v1 = poly[0]
-                v2 = poly[i]
+                v_1 = poly[0]
+                v_2 = poly[i]
 
-        if v1[1] > v2[1]:
+        if v_1[1] > v_2[1]:
             # y1 > y2
-            if v2[0] - v1[0] > v1[1] - v2[1]:
-                # case 1 
+            if v_2[0] - v_1[0] > v_1[1] - v_2[1]:
+                # case 1
                 #  \___
-                v3 = [v1[0] + v1[1] - v2[1], v2[1]]
+                v_3 = [v_1[0] + v_1[1] - v_2[1], v_2[1]]
             else:
                 # case 2
                 #  \
                 #   |
-                v3 = [v2[0], v1[0] - v2[0] + v1[1]]
+                v_3 = [v_2[0], v_1[0] - v_2[0] + v_1[1]]
         else:
             # y1 <= y2
-            if v2[0] - v1[0] > v2[1] - v1[1]:
+            if v_2[0] - v_1[0] > v_2[1] - v_1[1]:
                 # case 3
                 #  ___
                 # /
-                v3 = [v1[0] + v2[1] - v1[1], v2[1]]
+                v_3 = [v_1[0] + v_2[1] - v_1[1], v_2[1]]
             else:
                 # case 4
                 #  |
                 # /
-                v3 = [v2[0], v1[1] + v2[0]  - v1[0]]
-        v3s.append(v3)
+                v_3 = [v_2[0], v_1[1] + v_2[0]  - v_1[0]]
+        v3s.append(v_3)
     for i in range(segments):
         poly.insert(2 * i + 1, v3s[i])
     return poly
 
 def fix_direction_square(poly):
+    """fix direction to square like"""
     # input is a polygon contour
     segments = len(poly)
-    v3s = list()
+    v_3_s = []
     for i in range(segments):
         # get v1, v2 with v1 is at left (lower x)
         if i != segments-1:
             if poly[i][0] < poly[i + 1][0]:
-                v1 = poly[i]
-                v2 = poly[i + 1]
+                v_1 = poly[i]
+                v_2 = poly[i + 1]
             else:
-                v1 = poly[i + 1]
-                v2 = poly[i]
+                v_1 = poly[i + 1]
+                v_2 = poly[i]
         else:
             if poly[i][0] < poly[0][0]:
-                v1 = poly[i]
-                v2 = poly[0]
+                v_1 = poly[i]
+                v_2 = poly[0]
             else:
-                v1 = poly[0]
-                v2 = poly[i]
+                v_1 = poly[0]
+                v_2 = poly[i]
 
-        if v1[1] > v2[1]:
+        if v_1[1] > v_2[1]:
             # y1 > y2
-            if v2[0] - v1[0] > v1[1] - v2[1]:
-                # case 1 
+            if v_2[0] - v_1[0] > v_1[1] - v_2[1]:
+                # case 1
                 #  \___
-                v3 = [v1[0], v2[1]]
+                v_3 = [v_1[0], v_2[1]]
             else:
                 # case 2
                 #  \
                 #   |
-                v3 = [v2[0], v1[1]]
+                v_3 = [v_2[0], v_1[1]]
         else:
             # y1 <= y2
-            if v2[0] - v1[0] > v2[1] - v1[1]:
+            if v_2[0] - v_1[0] > v_2[1] - v_1[1]:
                 # case 3
                 #  ___
                 # /
-                v3 = [v1[0], v2[1]]
+                v_3 = [v_1[0], v_2[1]]
             else:
                 # case 4
                 #  |
                 # /
-                v3 = [v2[0], v1[1]]
-        v3s.append(v3)
+                v_3 = [v_2[0], v_1[1]]
+        v_3_s.append(v_3)
     for i in range(segments):
-        poly.insert(2 * i + 1, v3s[i])
+        poly.insert(2 * i + 1, v_3_s[i])
     return poly
--- svg.py
+++ svg.py
@@ -1,25 +1,30 @@
-# Scalable Vector Graphics (SVG) is an XML-based vector image format.
-# This file help to render image in svg format.
+"""
+Scalable Vector Graphics (SVG) is an XML-based vector image format.
+This file help to render image in svg format.
+"""
 
 def hex_str(value: int) -> str:
+    """convert hex value to string"""
     return '0' + hex(value)[2:] if value < 16 else hex(value)[2:]
 
 def rgb2hex(rgb: tuple) -> str:
+    """convert rgb color to hex color"""
     return '#' + hex_str(rgb[0]) + hex_str(rgb[1]) + hex_str(rgb[2])
 
-def to_svg(height, width, ps, cs, average_color, file_path) -> None:
-    svg_content = ['<svg xmlns="http://www.w3.org/2000/svg" viewBox="0.0 0.0 {} {}" \
-    height="{}px" width="{}px">\n'.format(width, height, height, width)]
+def to_svg(size, points, colors, average_color, file_path) -> None:
+    """generate svg path given polygon contour"""
+    height, width = size[0], size[1]
+    svg_content = ['<svg xmlns="http://www.w3.org/2000/svg" viewBox="0.0 0.0 {w} {h}" \
+    height="{h}px" width="{w}px">\n'.format(w = width, h = height)]
     path_template = '<path fill="{}" stroke-opacity="0" d="{}"></path>\n'
-    svg_content.append(path_template.format(rgb2hex(average_color), "M0 0 L0 {} L{} {} L{} 0 Z".format(height, width, height, width)))
-    for i in range(len(ps)):
-        sps = ps[i]
-        hex_color = rgb2hex((int(cs[i][0]), int(cs[i][1]), int(cs[i][2])))
-        path = "M{} {}".format(sps[0][0], sps[0][1])
+    svg_content.append(path_template.format(rgb2hex(average_color), \
+        f"M0 0 L0 {height} L{width} {height} L{width} 0 Z"))
+    for i, sps in enumerate(points):
+        hex_color = rgb2hex((int(colors[i][0]), int(colors[i][1]), int(colors[i][2])))
+        path = f"M{sps[0][0]} {sps[0][1]}"
         for each_point in sps[1:]:
-            path += " L{} {}".format(each_point[0], each_point[1])
+            path += f" L{each_point[0]} {each_point[1]}"
         svg_content.append(path_template.format(hex_color, path))
     svg_content.append("</svg>")
-    with open(file_path, 'w') as ww:
-        ww.writelines(svg_content)
-
+    with open(file_path, 'w', encoding='UTF-8') as write_handle:
+        write_handle.writelines(svg_content)
